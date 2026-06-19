02dcabdb17b121cdceaf4c2295d02fab567a7ebc
Harsh Singh <harsh0803singh@gmail.com>
2026-06-19 20:07:59 +0530

    Changes in the program

--- demo.java
+++ demo.java
@@ -1,174 +1,174 @@
-// public class demo {
-//    public static void main(String[] args) {
-//          int a = 5;
-//          int b = 3;
-//          int c = 2;
+public class demo {
+   public static void main(String[] args) {
+         int a = 5;
+         int b = 3;
+         int c = 2;
 
-//          int sum = a + b + c ;
-//          System.out.println(sum);
-//      }
-// }
-// import java.util.Scanner;
+         int sum = a + b + c ;
+         System.out.println(sum);
+     }
+}
+import java.util.Scanner;
 
-// public class demo {
-//    public static void main(String[] args) {
-//          Scanner S = new Scanner(System.in);
+public class demo {
+   public static void main(String[] args) {
+         Scanner S = new Scanner(System.in);
 
-//          System.out.println("Enter the name");
-//          String name = S.nextLine();
+         System.out.println("Enter the name");
+         String name = S.nextLine();
 
-//          System.out.println("Name : " + name);
+         System.out.println("Name : " + name);
 
-//          S.close();
-//      }
-// }
+         S.close();
+     }
+}
 
-// Write a program to calculate the CGPA of the student by using marks of the Three Subjects : (OUT OF 100)
-
-
-// import java.util.Scanner;
-
-// public class demo {
-//    public static void main(String[] args) {
-//          Scanner s = new Scanner(System.in);
-//          System.out.println("Enter the marks of the first subject : ");
-//          int marks1 = s.nextInt();
-
-//         System.out.println("Enter the marks of the second subject");
-//         int marks2 = s.nextInt();
-
-//         System.out.println("Enter the marks of the third subject");
-//         int marks3 = s.nextInt();
-
-//         double percentage = (marks1 + marks2 + marks3) / 3.0 ;
-//         double cgpa = percentage/9.5;
-
-//         System.out.println("The final result is : " + cgpa);
-//         s.close();
-
-//      }
-// }
+Write a program to calculate the CGPA of the student by using marks of the Three Subjects : (OUT OF 100)
 
 
-// :
+import java.util.Scanner;
 
-// import java.util.Scanner;
+public class demo {
+   public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+         System.out.println("Enter the marks of the first subject : ");
+         int marks1 = s.nextInt();
 
-// public class demo {
-//    public static void main(String[] args) {
+        System.out.println("Enter the marks of the second subject");
+        int marks2 = s.nextInt();
+
+        System.out.println("Enter the marks of the third subject");
+        int marks3 = s.nextInt();
+
+        double percentage = (marks1 + marks2 + marks3) / 3.0 ;
+        double cgpa = percentage/9.5;
+
+        System.out.println("The final result is : " + cgpa);
+        s.close();
+
+     }
+}
+
+
+:
+
+import java.util.Scanner;
+
+public class demo {
+   public static void main(String[] args) {
     
-//     Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
     
-//     System.err.println("Enter your Name : ");
-//     String name = s.nextLine();
+    System.err.println("Enter your Name : ");
+    String name = s.nextLine();
 
-//     System.out.println("Hello " + name + " Have a good day !");
+    System.out.println("Hello " + name + " Have a good day !");
      
-//     s.close();
-//      }
-// }
+    s.close();
+     }
+}
 
 
-// Write A java program to convert kilometers to miles :
+Write A java program to convert kilometers to miles :
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class demo {
-//    public static void main(String[] args) {
+public class demo {
+   public static void main(String[] args) {
          
-//     Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
 
-//     System.out.println("Enter the value of Kilometers : ");
-//     int kilo = s.nextInt();
+    System.out.println("Enter the value of Kilometers : ");
+    int kilo = s.nextInt();
 
-//     double miles = kilo * 0.621371 ;
+    double miles = kilo * 0.621371 ;
 
-//     System.out.println("The converted value of the kilometers in miles is : " + miles);
-//     s.close();
-//      }
-// }
+    System.out.println("The converted value of the kilometers in miles is : " + miles);
+    s.close();
+     }
+}
 
-// Write a java program to detect whether a given number is Integer or Not :
+Write a java program to detect whether a given number is Integer or Not :
 
-// import java.util.Scanner ;
+import java.util.Scanner ;
 
-// public class demo {
-//    public static void main(String[] args) {
+public class demo {
+   public static void main(String[] args) {
          
-//     Scanner s = new Scanner(System.in);
+    Scanner s = new Scanner(System.in);
 
-//     System.out.println("Enter the Number to check whether the given number is interger or not");
+    System.out.println("Enter the Number to check whether the given number is interger or not");
 
-//     if (s.hasNextInt())  {
-//         int num = s.nextInt();
-//         System.out.println("Number " + num + " is Integer");
-//     } else {
-//         System.out.println("Number is not Integer");
-//     }
-//     s.close();
-//      }
-// }
-
-
-//Type casting :
-
-//--> Convert data type of one variable into other data type :
-
-// Example Program :
-
-// import java.util.Scanner;
-
-// public class demo {
-//    public static void main(String[] args) {
-//          Scanner s = new Scanner(System.in);
-
-//          System.out.println("Enter your percentage");
-//          double percentage = s.nextDouble();
-
-//          float roundValue = (float) percentage;
-
-//          System.out.println("Interger value = " + roundValue);
-//          s.close();
-//      }
-// }
+    if (s.hasNextInt())  {
+        int num = s.nextInt();
+        System.out.println("Number " + num + " is Integer");
+    } else {
+        System.out.println("Number is not Integer");
+    }
+    s.close();
+     }
+}
 
 
-// Write a program to calculate the simple interest from taking the user input :
+Type casting :
 
-// import java.util.Scanner;
+--> Convert data type of one variable into other data type :
 
-// public class demo {
-//    public static void main(String[] args) {
-//          Scanner s = new Scanner(System.in);
+Example Program :
 
-//          System.out.println("Enter the Principle Amount : ");
-//          int principleAmount = s.nextInt();
+import java.util.Scanner;
 
-//          System.out.println("Enter the Rate of Interest : ");
-//          double rateOfInterest = s.nextDouble();
+public class demo {
+   public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
 
-//          System.out.println("Enter no. of Years : ");
-//          int years = s.nextInt();
+         System.out.println("Enter your percentage");
+         double percentage = s.nextDouble();
 
-//          double result = ( principleAmount * rateOfInterest * years ) / 100 ;
+         float roundValue = (float) percentage;
 
-//          System.out.println("The Simple Interest is : " + result);
-
-//          s.close();
-//      }
-// }
+         System.out.println("Interger value = " + roundValue);
+         s.close();
+     }
+}
 
 
-// What will be the result of the following expression :
-// float a = 7/4 * 9/2 ;
+Write a program to calculate the simple interest from taking the user input :
+
+import java.util.Scanner;
+
+public class demo {
+   public static void main(String[] args) {
+         Scanner s = new Scanner(System.in);
+
+         System.out.println("Enter the Principle Amount : ");
+         int principleAmount = s.nextInt();
+
+         System.out.println("Enter the Rate of Interest : ");
+         double rateOfInterest = s.nextDouble();
+
+         System.out.println("Enter no. of Years : ");
+         int years = s.nextInt();
+
+         double result = ( principleAmount * rateOfInterest * years ) / 100 ;
+
+         System.out.println("The Simple Interest is : " + result);
+
+         s.close();
+     }
+}
 
 
-// public class demo {
-//    public static void main(String[] args) {
-//          System.out.println(" The answer of the float is : ");
-//          float a = 7/4 * 9/2 ;
+What will be the result of the following expression :
+float a = 7/4 * 9/2 ;
 
-//          System.out.println("The answer of the A is " + a);
-//      }
-// }
+
+public class demo {
+   public static void main(String[] args) {
+         System.out.println(" The answer of the float is : ");
+         float a = 7/4 * 9/2 ;
+
+         System.out.println("The answer of the A is " + a);
+     }
+}
 
